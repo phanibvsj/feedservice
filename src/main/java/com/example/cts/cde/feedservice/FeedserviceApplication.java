9e02f7e0faeac30d15e71dbbd8f1019d0e198bfb
B V S J <643017@cognizant.com>
2017-10-27 15:21:56 +0530

    initial commit

---
+++ src/main/java/com/example/cts/cde/feedservice/FeedserviceApplication.java
@@ -0,0 +1,12 @@
+package com.example.cts.cde.feedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedserviceApplication.class, args);
+	}
+}
